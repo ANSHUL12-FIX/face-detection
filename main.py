a3de8e268eefc7eb1f2f54aad132824427f128d4
ANSHUL12-FIX <64637596+ANSHUL12-FIX@users.noreply.github.com>
2022-07-19 19:55:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#it is used for ml like image processing,video processiong
+#python library use for working with array
+#it is a system that identifies face
+import cv2
+import numpy as np
+import face_recognition
+
+#here we are loading the img file
+#and here we are converting image bgr to rgb format
+imgElon = face_recognition.load_image_file('imageBasic/Elon Musk.jpg')
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file('imageBasic/Elon Test.jpg')
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+faceDis = face_recognition.face_distance([encodeElon],encodeTest)
+print(results,faceDis)
+cv2.putText(imgTest,f'{results} {round(faceDis[0],2)}',(25,25),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,200),2)
+
+
+
+cv2.imshow('Elon Musk',imgElon)
+cv2.imshow('Elon Test',imgTest)
+cv2.waitKey()
